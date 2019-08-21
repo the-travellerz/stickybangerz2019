f630a67bec667b7b9ff8695b04a376131e3f27ea
stickybangerz <54333665+stickybangerz@users.noreply.github.com>
2019-08-20 20:39:08 -0400

    movement script created

--- Sticky Bangerz/Assets/MoveMent.cs
+++ Sticky Bangerz/Assets/MoveMent.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class MoveMent : MonoBehaviour
 {
+    // GET HANDLE for rigid body
+    private Rigidbody2D _rigid2D;
+    public float speedMultipliyer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //assign handle for rigid body
+        _rigid2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // set horizontal inputs for left and right
+
+        float horitonalInput = Input.GetAxis("Horizontal");
+        //velocity
+
+        _rigid2D.velocity = new Vector2(horitonalInput*speedMultipliyer, _rigid2D.velocity.y);
+
     }
 }
---
+++ Sticky Bangerz/Assets/jump.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+    private BoxCollider2D collider2D;
+    [SerializeField]
+    public float jumpforce;
+    public bool isGrounded = true;
+    public float raycastdistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        collider2D = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Jump();
+
+        }
+
+        Grounded();
+        OnCollisionEnter();
+    }
+
+    void Jump()
+    {
+        // jump function
+        // is grounded
+        if (isGrounded)
+        {
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpforce);
+            isGrounded = false;
+        }
+        
+    }
+
+    private void Grounded()
+    {
+        RaycastHit2D hitinfo = Physics2D.Raycast(transform.position, Vector2.down, raycastdistance);
+
+        if (hitinfo.collider != null)
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+
+
+
+    }
+
+
+}
